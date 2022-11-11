36db8f1ef567808e17c714939676b657bc0f0763
Andrey Skorodumov <quickandrey@gmail.com>
2022-11-10 16:32:17 -0800

    Bugfixes and updated to meet requirements

--- script.js
+++ script.js
@@ -8,39 +8,47 @@ var upperCharacters = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L
 
 //User Inputs
 
+var upperCase = ""
+var specialCase = ""
+var numberCase = ""
+var passwordPool = []
+
 let lowerCase = true
-let upperCase = confirm("Would you like to include upper case letters in your password?")
-let specialCase = confirm("Would you like to include special characters in your password?")
-let numberCase = confirm("Would you like to include numbers in your password?")
-var passwordLength = prompt("How long would you like your password to be? (Between 8 and 128)")
+function questions() {
+  upperCase = confirm("Would you like to include upper case letters in your password?")
+  specialCase = confirm("Would you like to include special characters in your password?")
+  numberCase = confirm("Would you like to include numbers in your password?")
+  passwordLength = prompt("How long would you like your password to be? (Between 8 and 128)")
+  //Password length verify
+  while (passwordLength < 8 || passwordLength > 128) { passwordLength = prompt("Please input a password length between 8 and 128.") }
+  while (isNaN(passwordLength) == true || passwordLength === "") { passwordLength = prompt("Please input a number."); }
+  //Password Pool
+  function generatePool() {
+    passwordPool = []
+    if (lowerCase) { passwordPool = passwordPool.concat(lowerCharacters); } else { }
+    if (upperCase) { passwordPool = passwordPool.concat(upperCharacters) } else { }
+    if (numberCase) { passwordPool = passwordPool.concat(numCharacters) } else { }
+    if (specialCase) { passwordPool = passwordPool.concat(specialCharacters) } else { }
 
-//Password length verify
-while (passwordLength < 8 || passwordLength > 128) { passwordLength = prompt("Please input a password length between 8 and 128.") }
-while (isNaN(passwordLength) == true || passwordLength === "") { passwordLength = prompt("Please input a number."); }
+    return passwordPool;
+  }
 
-//Password Pool
-function generatePool() {
-  var passwordPool = []
-  if (lowerCase) { passwordPool = passwordPool.concat(lowerCharacters); } else { }
-  if (upperCase) { passwordPool = passwordPool.concat(upperCharacters) } else { }
-  if (numberCase) { passwordPool = passwordPool.concat(numCharacters) } else { }
-  if (specialCase) { passwordPool = passwordPool.concat(specialCharacters) } else { }
-
-  return passwordPool;
+  passwordPool = generatePool()
 }
 
-var passwordPool = generatePool()
+
 
 //Generate password
 
 var password = []
 function generatePassword() {
-
+  password = []
   for (let i = 0; i < Math.floor(passwordLength); i++) {
     randomSelect = Math.floor(Math.random() * passwordPool.length)
     passCharacter = passwordPool[randomSelect]
     password = password.concat(passCharacter)
   }
+
 }
 
 // Assignment Code
@@ -48,7 +56,7 @@ var generateBtn = document.querySelector("#generate");
 
 // Write password to the #password input
 function writePassword() {
-  clearPasswordText()
+  questions()
   generatePassword()
   var passwordOutput = password.join('')
   console.log(passwordOutput);
